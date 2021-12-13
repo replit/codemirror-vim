698c0e79782c20048cd587f0c5d322bfc12b0c99
lishid <lishid@gmail.com>
2021-12-13 15:53:20 -0500

    Fix up/down navigation sometimes jumps 2 lines when line height isn't uniform.

--- src/cm_adapter.ts
+++ src/cm_adapter.ts
@@ -502,19 +502,22 @@ export class CodeMirror {
     };
   };  
   findPosV(start: Pos, amount: number, unit: "page"|"line", goalColumn?:number) {
-    var doc = this.cm6.state.doc;
-    let pixels = 0;
-    
-    if (unit == 'page') {
-      pixels = this.cm6.dom.clientHeight
-    } else if (unit == 'line') {
-      pixels = this.cm6.defaultLineHeight
+    let {cm6} = this;
+    const doc = cm6.state.doc;
+    let pixels = unit == 'page' ? cm6.dom.clientHeight : 0;
+
+    const startOffset = indexFromPos(doc, start);
+    let range = EditorSelection.range(startOffset, startOffset, goalColumn);
+    for (let i = 0; i < Math.abs(amount); i++) {
+      if (unit == 'page') {
+        range = cm6.moveVertically(range, amount > 0, pixels);
+      }
+      else if (unit == 'line') {
+        range = cm6.moveVertically(range, amount > 0);
+      }
     }
-    var startOffset = indexFromPos(doc, start);
-    var range = EditorSelection.range(startOffset, startOffset, goalColumn);
-    var newSelection = this.cm6.moveVertically(range, amount > 0, pixels * Math.abs(amount));
-    var newOffset = newSelection.head;
-    return posFromIndex(doc, newOffset);
+
+    return posFromIndex(doc, range.head);
   };
   charCoords(pos: Pos, mode: "div"| "local") {
     var rect = this.cm6.contentDOM.getBoundingClientRect();

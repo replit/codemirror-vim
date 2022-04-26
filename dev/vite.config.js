535defa55ff6275b58ecd186b4acfe9b6b03c8dc
Faris Masad <farismasad@gmail.com>
2022-04-26 17:36:06 +0000

    Fix HMR on Replit fixed #11

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -1,20 +1,24 @@
 import { fileURLToPath } from 'url';
-import * as path from "path"
+import * as path from 'path';
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
+const isReplit = Boolean(process.env.REPL_SLUG);
+
 export default {
   server: {
     host: '0.0.0.0',
-    /*hmr: {
-      port: 443,
-    }*/
+
+    ...(isReplit
+      ? {
+          hmr: {
+            clientPort: 443,
+          },
+        }
+      : {}),
   },
-  resolve:{
-    alias:[
-        { find: /^..$/, replacement:  dirname + '/../src/index.ts' },
-      ]
-    
+  resolve: {
+    alias: [{ find: /^..$/, replacement: dirname + '/../src/index.ts' }],
   },
-}
+};

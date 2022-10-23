e280b13eb54b3dfd62e96ba15461040e52b4e310
nightwing <amirjanyan@gmail.com>
2022-10-24 01:25:53 +0400

    fix cursor on emojis in visual mode

--- src/block-cursor.ts
+++ src/block-cursor.ts
@@ -148,6 +148,12 @@ function measureCursor(cm: CodeMirror, view: EditorView, cursor: SelectionRange,
   }
 
   if (fatCursor) {
+    let letter = head < view.state.doc.length && view.state.sliceDoc(head, head + 1);
+    if (letter && (/[\uDC00-\uDFFF]/.test(letter) && head > 1)) {
+      // step back if cursor is on the second half of a surrogate pair
+      head--;
+      letter = view.state.sliceDoc(head, head + 1);
+    }
     let pos = view.coordsAtPos(head, 1);
     if (!pos) return null;
     let base = getBase(view);
@@ -162,7 +168,6 @@ function measureCursor(cm: CodeMirror, view: EditorView, cursor: SelectionRange,
       node = node.parentNode;
     }
     let style = getComputedStyle(node as HTMLElement);
-    let letter = head < view.state.doc.length && view.state.sliceDoc(head, head + 1);
     if (!letter || letter == "\n" || letter == "\r") letter = "\xa0";
     else if ((/[\uD800-\uDBFF]/.test(letter) && head < view.state.doc.length - 1)) {
       // include the second half of a surrogate pair in cursor

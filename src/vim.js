06001da259f35d0eb52b69bb13c24cf533d987c4
Mathias Jakobsen <mathias.jakobsen@overleaf.com>
2022-11-30 09:49:41 +0000

    Reset global jumpList on unload

--- src/vim.js
+++ src/vim.js
@@ -299,6 +299,7 @@ export function initVim(CodeMirror) {
       cm.off('cursorActivity', onCursorActivity);
       CodeMirror.off(cm.getInputField(), 'paste', getOnPasteFn(cm));
       cm.state.vim = null;
+      vimGlobalState.jumpList = createCircularJumpList();
       if (highlightTimeout) clearTimeout(highlightTimeout);
     }
 

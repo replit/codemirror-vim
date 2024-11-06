b0bfa432f41adfbcd697930a291cb7f9d43a68f0
Harutyun Amirjanyan <amirjanyan@gmail.com>
2024-11-06 23:25:32 +0400

    Verymagic (#190)
    * fix substitution with empty string
    
    * fix input dialog overlapping key info
    
    * support backreferences in replacement pattern
    
    * improve vim regex support
    
    * better explain reason why when regex doesn't match
    
    * Update src/vim.js
    
    Co-authored-by: Faris Masad <farismasad@gmail.com>
    
    * Update src/vim.js
    
    Co-authored-by: Faris Masad <farismasad@gmail.com>
    
    * fix tests
    
    ---------
    
    Co-authored-by: Faris Masad <farismasad@gmail.com>

--- test/vim_test.js
+++ test/vim_test.js
@@ -4488,7 +4488,7 @@ testVim('ex_substitute_javascript', function(cm, vim, helpers) {
   cm.setCursor(1, 0);
   // Throw all the things that javascript likes to treat as special values
   // into the replace part. All should be literal (this is VIM).
-  helpers.doEx('s/\\(\\d+\\)/$$ $\' $` $& \\1/g')
+  helpers.doEx('s/\\(\\d\\+\\)/$$ $\' $` $& \\1/g')
   eq('a $$ $\' $` $& 0 b', cm.getValue());
 
   cm.setValue('W12345678OR12345D');
@@ -4509,6 +4509,28 @@ testVim('ex_substitute_highlight', function(cm,vim,helpers) {
   helpers.doKeys('\n');
   is(!searchHighlighted(vim));
 }, {value: 'a a\na a'});
+testVim('ex_substitute_nopcre_special', function(cm, vim, helpers) {
+  CodeMirror.Vim.setOption('pcre', false);
+
+  cm.setValue('aabb1cxyz$^o aabb2cxyz$^o aabb3cxyz$^o aabb4cxyz$^o ');
+  helpers.doEx(
+    's/'
+    + '\\v<a*(b|\\d){3}c?[x-z]+\\$\\^.> '
+    + '\\V\\<a\\*\\(b\\|\\d\\)\\{3\\}c\\?\\[x-z]\\+$^\\.\\> '
+    + '\\m\\<a*\\(b\\|\\d\\)\\{3}c\\?[x-z]\\+\\$\\^.\\> '
+    + '\\M\\<a\\*\\(b\\|\\d\\)\\{3}c\\?\\[x-z]\\+\\$\\^\\.\\>'
+    + '/M\\4 m\\3 V\\2 v\\1/'
+  );
+  eq('M4 m3 V2 v1 ', cm.getValue());
+  
+  cm.setValue('10 12 13 42');
+  helpers.doEx('s/\\m\\(1\\)\\v\\ze(\\d+)/\\2\\1 a\\1/g')
+  eq('01 a10 21 a12 31 a13 42', cm.getValue());
+  helpers.doEx('s/2\\zs\\>/b/g');
+  eq('01 a10 21 a12b 31 a13 42b', cm.getValue());
+  helpers.doEx('s/\\m\\<\\d\\+ //g');
+  eq('a10 a12b a13 42b', cm.getValue());
+}, { value: '' });
 
 // More complex substitute tests that test both pcre and nopcre options.
 function testSubstitute(name, options) {
@@ -4533,22 +4555,22 @@ testSubstitute('ex_substitute_capture', {
   // $n is a backreference
   expr: 's/(\\d+)/$1$1/g',
   // \n is a backreference.
-  noPcreExpr: 's/\\(\\d+\\)/\\1\\1/g'});
+  noPcreExpr: 's/\\(\\d\\+\\)/\\1\\1/g'});
 testSubstitute('ex_substitute_capture2', {
   value: 'a 0 b',
   expectedValue: 'a $00 b',
   expr: 's/(\\d+)/$$$1$1/g',
-  noPcreExpr: 's/\\(\\d+\\)/$\\1\\1/g'});
+  noPcreExpr: 's/\\(\\d\\+\\)/$\\1\\1/g'});
 testSubstitute('ex_substitute_nocapture', {
   value: 'a11 a12 a13',
   expectedValue: 'a$1$1 a$1$1 a$1$1',
   expr: 's/(\\d+)/$$1$$1/g',
-  noPcreExpr: 's/\\(\\d+\\)/$1$1/g'});
+  noPcreExpr: 's/\\(\\d\\+\\)/$1$1/g'});
 testSubstitute('ex_substitute_nocapture2', {
   value: 'a 0 b',
   expectedValue: 'a $10 b',
   expr: 's/(\\d+)/$$1$1/g',
-  noPcreExpr: 's/\\(\\d+\\)/\\$1\\1/g'});
+  noPcreExpr: 's/\\(\\d\\+\\)/\\$1\\1/g'});
 testSubstitute('ex_substitute_nocapture', {
   value: 'a b c',
   expectedValue: 'a $ c',
@@ -4694,7 +4716,7 @@ testSubstitute('ex_substitute_empty_match', {
   value: 'aaa  aa\n aa\nbb\n',
   expectedValue: '<aaa>  <aa>\n <aa>\nbb<>\n<>',
   expr: '%s/(a+|$)/<$1>/g',
-  noPcreExpr: '%s/\\(a+\\|$\\)/<\\1>/g'});
+  noPcreExpr: '%s/\\(a\\+\\|$\\)/<\\1>/g'});
 testSubstitute('ex_substitute_empty_or_match', {
   value: '1234\n567\n89\n0\n',
   expectedValue: '<12><34>\n<56>7<>\n<89>\n0<>\n<>',
